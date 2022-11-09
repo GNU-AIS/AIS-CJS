22bfbbebdeb4e48cd090388db82d4d7291771c4f
도훈 차 <cha_dh1004@naver.com>
2022-11-09 15:16:11 +0900

    프로그램 창 구현

--- AIS-CJS/jobSearch.py
+++ AIS-CJS/jobSearch.py
@@ -1,2 +1,21 @@
-print("부끄럽다")
-print("시발")
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Job Search')
+        self.move(550, 400)
+        self.resize(1500, 800)
+        self.show()
+
+
+if __name__ == '__main__':
+   app = QApplication(sys.argv)
+   ex = MyApp()
+   sys.exit(app.exec_())
