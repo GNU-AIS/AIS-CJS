0448b8fb05e1e4f0903a1f61879d8bb603cfc496
KoalitsiyaSV <chyoung118@gmail.com>
2022-11-16 17:24:47 +0900

    임시 DB 연결

--- AIS-CJS/CJS_Flask_Server/__init__.py
+++ AIS-CJS/CJS_Flask_Server/__init__.py
@@ -1,8 +1,12 @@
+import pymysql
 from flask import Flask
+
+conn = pymysql.connect(host='localhost', user='root', password='1234', db='BBS', charset='utf8') # mysql 연결
+curs = conn.cursor() # sql 문 입력용 cursor 생성
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello world"
+    return "Hello World"
