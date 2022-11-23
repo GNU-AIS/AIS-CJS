56517ddd1f49a8337352b5acab68b15e7bcd2fa1
도훈 차 <cha_dh1004@naver.com>
2022-11-23 17:50:34 +0900

    1번 크롤링 완성

--- AIS-CJS/jobSearch.py
+++ AIS-CJS/jobSearch.py
@@ -8,9 +8,9 @@ def main():
     # 잡코리아
 
     f = open('jobkorea_apply_urls.csv', 'w')
-    f.write("기업 이름,모집 제목,경력,학력,우대,고용 형태, 급여, 지역, 모집 기간" + '\n')
+    f.write("기업 이름,모집 제목,경력,학력,우대,고용 형태, 급여, 지역, 모집 기간, 이미지 주소" + '\n')
 
-    keyword = "프론트엔드"  # 키워드 입력
+    keyword = "모바일앱"  # 키워드 입력
 
     # 페이지순서
     for n in range(1, 2):
@@ -38,18 +38,52 @@ def main():
             #score = str(html2.select("#tab04 > article.artReadStrategy > div > div > div.devStartlist.listArea.specList > div > div.specListWrap > div > ul > li:nth-child(1) > div > span > em"))[5:8]
             date_tag1 = html2.select("#tab02 > div.divReadBx.clear.devMakeSameHeight > article.artReadPeriod > div > dl.date > dd:nth-child(2)")
             date_tag2 = html2.select("#tab02 > div.divReadBx.clear.devMakeSameHeight > article.artReadPeriod > div > dl.date > dd:nth-child(4)")
+            prefer_tag = html2.select("#dlPref > dd > span")
+            region_tag = html2.select("#container > section > div.readSumWrap.clear > article > div.tbRow.clear > div:nth-child(2) > dl > dd:nth-child(6) > a")
+            pay_tag = html2.select("#container > section > div.readSumWrap.clear > article > div.tbRow.clear > div:nth-child(2) > dl > dd:nth-child(4)")
+            edu_tag = html2.select("#container > section > div.readSumWrap.clear > article > div.tbRow.clear > div:nth-child(1) > dl > dd:nth-child(4) > strong")
+            pattern_tag = html2.select("#container > section > div.readSumWrap.clear > article > div.tbRow.clear > div:nth-child(2) > dl > dd:nth-child(2) > ul > li > strong")
+            image_tag = html2.select("#cologo")
+
+            image = ""
+            for i in image_tag:
+                image = i['src']
+            if image == "":
+                image = "이미지 없음"
+            else:
+                image = "http:" + image
+            date = ""
+            pay = ""
             prefer = ""
             region = ""
-            pay = ""
-            date = ""
             edu = ""
             pattern = ""
             for tag in date_tag1:
                 date += "시작일 " + tag.getText() +" "
             for tag in date_tag2:
                 date += "마감일 " + tag.getText()
+            for tag in prefer_tag:
+                prefer += tag.getText()
+            for tag in region_tag:
+                region += tag.getText()
+            for tag in pay_tag:
+                pay += tag.getText()
+            for tag in edu_tag:
+                edu += tag.getText()
+            for tag in pattern_tag:
+                pattern += tag.getText()
+
+            pay = remove_blank(pay)
+            pay = pay.replace(",","")
+            region = remove_blank(region)
+            prefer = remove_blank(prefer)
+            pattern = remove_blank(pattern)
+            edu = remove_blank(edu)
+            date = remove_blank(date)
+            if date == "":
+                date = "상시 채용"
             f.write(
-                company_name + ',' + detail + ','+ exp + ','+ edu + ',' + prefer + ',' + pattern  + ',' + pay + ',' + region + ','  +  date + '\n')
+                company_name + ',' + detail + ',' + exp + ',' + edu + ',' + prefer + ',' + pattern  + ',' + pay + ',' + region + ',' + date +  ',' + image + '\n')
             time.sleep(1)  # 1초
 
 
@@ -61,6 +95,15 @@ def main():
 
 
     print("잡코리아 크롤링이 완료되었습니다!!")
+
+
+def remove_blank(string):
+    string = string.strip()
+    string = string.replace("\n", "")
+    string = string.replace("\r", "")
+    return string
+
+
 
 if __name__ == "__main__":
     main()
