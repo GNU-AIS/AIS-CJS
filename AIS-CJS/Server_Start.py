ea544ec4a4fa2c06a336233afd4985b55949538a
KoalitsiyaSV <chyoung118@gmail.com>
2022-11-23 15:18:45 +0900

    Json 파일 형식으로 데이터 전송

--- AIS-CJS/Server_Start.py
+++ AIS-CJS/Server_Start.py
@@ -1,27 +1,67 @@
 import pymysql
-from flask import Flask
+from flask import Flask, json, request, jsonify
 import datetime, json
 
-conn = pymysql.connect(host='localhost', user='root', password='1234', db='test', charset='utf8') # mysql 연결
+
+conn = pymysql.connect(host='localhost', user='root', password='1234', db='aisprojectdb', charset='utf8') # mysql 연결
 curs = conn.cursor() # sql문을 입력할 cursor 생성
 
-if conn.open: # DB 연결 여부 확인
+if conn.open: # DB 연결 여부
     print('connected')
 try:
     app = Flask(__name__) 
     # flask 메인 
     # __name__: 현재 이 파일을 실행하고 있는 파일의 이름이 들어감(원본과 같은 파일 일 경우 '__main__')
 
-    @app.route('/test',methods=['POST','GET'])
-    # route() : 외부 웹브라우져에서 웹서버로 접근 시 해당 주소로 입력을 하게 되면 특정 함수가 실행되게 도와줌('/test': /test 주소에 접근하면 아래 함수 실행)
+    @app.route('/output',methods=['POST','GET'])
 
-    def dbToWeb(): # DB에서 웹으로 데이터를 보내기위한 함수
-            sql = "select * from test" # DB에서 필요한 데이터 select하는 sql문
-            curs.execute(sql) # sql문 실행
-            rows = curs.fetchall() # select한 데이터 fetch (fetchall() : 모든 데이터 fetch)
-            return json.dumps(rows) 
-            # fetch한 데이터 json형식으로 변환 후 string으로 바꿔서 return
+    def dbtoweb(): # DB에서 웹으로 데이터를 보내기위한 함수
+        sql = "select * from testtbl" # DB에서 필요한 데이터 select하는 sql문
+        curs.execute(sql) # sql문 실행
+        rows = curs.fetchall() # select한 데이터 fetch (fetchall() : 모든 데이터 fetch)
+        columns = [desc[0] for desc in curs.description]
+        result = []
+        for row in rows:
+            row = dict(zip(columns, row))
+            result.append(row)
+        row = result
+        return row
+        # fetch한 데이터 json형식으로 변환 후 string으로 바꿔서 return
+
+    @app.route('/jpoutput',methods=['POST', 'GET'])
+
+    def dbtowebjp():
+        sql = "select * from jptbl"
+        curs.execute(sql)
+        rows = curs.fetchall()
+        return json.dumps(rows)
+
+    @app.route('/jpinput',methods=['POST','GET'])
+
+    def sendtodb():
+        company = request.json('company')
+        busino = request.json('busino')
+        title = request.json('title')
+        salTpNm = request.json('salTpNm')
+        minSal = request.json('minSal')
+        maxSal = request.json('maxSal')
+        region = request.json('region')
+        holidayTpNm = request.json('holidayTpNm')
+        minEdubg = request.json('minEdubg')
+        maxEdubg = request.json('maxEdubg')
+        career = request.json('career')
+        regDt = request.json('regDt')
+        closeDt = request.json('closeDt')
+        wantedInfoUrl = request.json('wantedInfoUrl')
+        wantedMobileInfoUrl = request.json('wantedMobileInfoUrl')
+        basicAddr = request.json('basicAddr')
+        empTpCd = request.json('empTpCd')
+        jobsCd = request.json('jobsCd')
+        smodifyDfm = request.json('smodifyDfm')
+        prefCd = request.json('prefCd')
+
+        return "Success"
 
 finally:
-    app.run(host='0.0.0.0', port= 5000) # 웹서버 호스트, 포트 지정
-    conn.close() # DB 연결 해제
+    app.run(host='0.0.0.0', port= 5000)
+    conn.close()
