4afe03c20740bd9aa2af78e492d3fa67c93f56c9
지홍 박 <hong_0002@naver.com>
2022-11-30 14:29:42 +0900

    Changes

--- AIS-CJS/AIS_CJS.py
+++ AIS-CJS/AIS_CJS.py
@@ -1,21 +1,19 @@
 # selenium Ver : 3.14.1
 from selenium import webdriver
+# pip install beautifulsoup4
+from bs4 import BeautifulSoup as bs
 
-# 네이버 URL
-url = "https:naver.com"
-# 네이버 검색창 Xpath
-xpath_text = '//*[@id="query"]'
-# 검색하기 버튼
-xpath_button = '//*[@id="search_btn"]'
-# 검색할 내용
-keyword = "셀레니움 웹크롤링"
-
-
-# 웹드라이버 열기
+# 웹드라이버 열기 (네이버에 현재상영작 검색 URL)
+url = "https://www.catch.co.kr/JobN/Pass/JobReport/05"
 driver = webdriver.Chrome()
 driver.get(url)
 
-# 검색 창에 keyword 입력
-driver.find_element_by_xpath(xpath_text).send_keys(keyword)
-# 검색 버튼 클릭하기기
-driver.find_element_by_xpath(xpath_button).click()
+# 크롬드라이버의 현재 페이지의 url 얻기
+page_url = driver.page_source
+# 현재 url 주소의 html 데이터를 파싱
+soup = bs(page_url, "html.parser")
+movie_list = soup.find_all(class_='ico next')
+
+# 8개의 영화 정보가 리스트로 저장되어 있습니다.
+for movie_info in movie_list:
+    print(movie_info.text)
--- AIS-CJS/PCS_Search.py
+++ AIS-CJS/PCS_Search.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 import sys
 
@@ -7,14 +8,21 @@ import sys
 # 네이버에서 종목 코드를 불러옴
 def search():
     url = "https://www.catch.co.kr/JobN/Pass/JobReport/05"
-    wd = webdriver.Chrome('./WebDriver/chromedriver.exe')
-    '''wd = webdriver.Chrome('C:/Users/hong/Desktop/chromedriver_win32/chromedriver.exe')'''
-
+    wd = webdriver.Chrome()
     wd.get(url)
-    xpath_button = '//*[@id="report_wrap4"]'
-    wd.find_element_by_link_text(xpath_button).click()
-    time.sleep(10)
 
+    # 크롬드라이버의 현재 페이지의 url 얻기
+    page_url = wd.page_source
+    xpath_button = '//*[@id="report_wrap4"]/div[2]/div[5]/p/a[12]'
+    i=0
+    wd.find_element(By.XPATH, xpath_button).click()
+    time.sleep(1000)
+    wd.find_element(By.XPATH, xpath_button).click()
+    time.sleep(1000)
+    wd.find_element(By.XPATH, xpath_button).click()
+    time.sleep(1000)
+    wd.find_element(By.XPATH, xpath_button).click()
+    time.sleep(1000)
     sys.stdout = open('stdout.txt', 'w', encoding='UTF-8')
     print(wd.page_source)
 
