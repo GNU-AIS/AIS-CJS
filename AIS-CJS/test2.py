2d2efe257c8dd22c7db5877c191d4cef62c24da4
지홍 박 <hong_0002@naver.com>
2022-12-21 11:05:52 +0900

    수정완료

--- AIS-CJS/test2.py
+++ AIS-CJS/test2.py
@@ -1,4 +1,7 @@
 import matplotlib as mpl
+import matplotlib.font_manager as fm
 print(mpl.matplotlib_fname())
 
 print(mpl.get_cachedir())
+font_list = fm.findSystemFonts(fontpaths=None, fontext='ttf')
+print(font_list)
