a53317b7a1ec3eb3bf03cc0cd4871644d9f0d0f6
도훈 차 <cha_dh1004@naver.com>
2022-12-21 12:14:38 +0900

    내가먹음 ㅅㄱ

--- AIS-CJS/wordCloud.py
+++ AIS-CJS/wordCloud.py
@@ -6,7 +6,11 @@ from wordcloud import WordCloud
 
 inputFileName = 'csvjson'
 data = json.loads(open(inputFileName+'.json', 'r', encoding= 'utf-8').read())
-wc = WordCloud(font_path="c:/Windows/fonts/malgun.ttf", background_color='ivory', width=800, height=600)
+font_path = "C:/Users/dofury/AppData/Local/Microsoft/Windows/Fonts/MaruBuri-Bold.ttf"
+wc = WordCloud(font_path=font_path, background_color='ivory', width=800, height=600)
+
+
+
 
 cloud = wc.fit_words(data)
 plt.Figure(figsize=(15, 20))
